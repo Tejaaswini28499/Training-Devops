1d585d5cba436bc6ca8e5111dd7daa7de3f730e0
Tejaaswini28499 <52596535+Tejaaswini28499@users.noreply.github.com>
2025-09-07 23:07:52 +0530

    Update python_code.py

--- Python/python_code.py
+++ Python/python_code.py
@@ -6,3 +6,25 @@ for i in range(52):
     total_expenses = total_expenses + weekly_expenses
     print(i+1, "week expenses =", total_expenses)
 print("total expenses for this month is", total_expenses )
+
+
+working_days = 23
+salary_per_day = 10000
+monthly_salary = 0
+
+for i in range(working_days):
+    monthly_salary = monthly_salary + salary_per_day
+    if(i%7 == 0):
+        print(i, "salary_per_day", monthly_salary)
+    
+print("my monthy salary is", monthly_salary)
+
+working_days = 23
+salary_per_day = 10000
+monthly_salary = 0
+
+for i in range(working_days):
+    monthly_salary = monthly_salary + salary_per_day
+    print(i+1, "salary_per_day", monthly_salary)
+    
+print("my monthy salary is", monthly_salary)
